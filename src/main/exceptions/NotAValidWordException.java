0f3bfd7f6c8c9bf2a9a3b633525d6e27cc7ac9bf
Olga <slota.olga@gmail.com>
2017-06-05 19:35:23 +0200

    Update NotAValidWordException.java

--- src/main/exceptions/NotAValidWordException.java
+++ src/main/exceptions/NotAValidWordException.java
@@ -2,6 +2,6 @@ package main.exceptions;
 
 public class NotAValidWordException extends Exception {
     public NotAValidWordException(String word) {
-        super("Given word is not in dictionary: " + word);
+        super("Given word is not in dictionary: " + word + ".");
     }
 }
